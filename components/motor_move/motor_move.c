debb974301b0763f746b051783e617aacc3b3fd0
Jan Górski <77403638+JanG175@users.noreply.github.com>
2023-08-30 15:14:25 +0200

    working encoders

--- components/motor_move/motor_move.c
+++ components/motor_move/motor_move.c
@@ -1,14 +1,35 @@
+/**
+ * @file motor_move.c
+ * @author JanG175
+ * @brief 6_DOF ROBOT ARM LIBRARY
+ * 
+ * @copyright All rigths reserved (R) 2023
+ */
+
 #include <stdio.h>
 #include "motor_move.h"
 
 static const char *TAG = "motor_move";
 
 
+/**
+ * @brief initialize all motors
+ * 
+ * @param AX_conf struct with AX servo parameters
+ * @param emm42_conf struct with emm42 servo parameters
+ * @param mks_conf struct with mks servo parameters
+ */
 void motor_init(AX_servo_conf_t AX_conf, emm42_conf_t emm42_conf, mks_conf_t mks_conf)
 {
     AX_servo_init(AX_conf);
     emm42_servo_init(emm42_conf);
     mks_servo_init(mks_conf);
+
+    // stop all motors
+    emm42_servo_uart_move(emm42_conf, 1, 0, 0, 0);
+    mks_servo_uart_cr_set_pos(mks_conf, 2, 0, 0, 0);
+
+    vTaskDelay(100 / portTICK_PERIOD_MS);
 }
 
 
--- main/main.c
+++ main/main.c
@@ -6,40 +6,44 @@
 #include "esp_log.h"
 #include "motor_move.h"
 
+#define FULL_ROT 200
+
+static const char *TAG = "main";
+
 
 void app_main(void)
 {
     AX_servo_conf_t AX_conf = {
-        .uart = UART_NUM_2,
-        .tx_pin = 17,
-        .rx_pin = 16,
+        .uart = UART_NUM_0,
+        .tx_pin = 1,
+        .rx_pin = 3,
         .rts_pin = 5,
         .baudrate = 115200
     };
 
-    gpio_num_t emm42_step_pin[] = {18, 19};
-    gpio_num_t emm42_dir_pin[] = {12, 14};
-    gpio_num_t emm42_en_pin[] = {27, 26};
+    gpio_num_t emm42_step_pin[] = {23, 14};
+    gpio_num_t emm42_dir_pin[] = {22, 13};
+    gpio_num_t emm42_en_pin[] = {21, 12};
 
     emm42_conf_t emm42_conf = {
-        .uart = UART_NUM_0,
+        .uart = UART_NUM_2,
         .baudrate = 115200,
-        .tx_pin = 1,
-        .rx_pin = 3,
+        .tx_pin = 17,
+        .rx_pin = 16,
         .step_pin = emm42_step_pin,
         .dir_pin = emm42_dir_pin,
         .en_pin = emm42_en_pin
     };
 
-    gpio_num_t mks_step_pin[] = {13};
-    gpio_num_t mks_dir_pin[] = {15};
+    gpio_num_t mks_step_pin[] = {27};
+    gpio_num_t mks_dir_pin[] = {26};
     gpio_num_t mks_en_pin[] = {25};
 
     mks_conf_t mks_conf = {
-        .uart = UART_NUM_0,
+        .uart = UART_NUM_2,
         .baudrate = 115200,
-        .tx_pin = 1,
-        .rx_pin = 3,
+        .tx_pin = 17,
+        .rx_pin = 16,
         .step_pin = mks_step_pin,
         .dir_pin = mks_dir_pin,
         .en_pin = mks_en_pin
@@ -47,9 +51,29 @@ void app_main(void)
 
     motor_init(AX_conf, emm42_conf, mks_conf);
 
+    int16_t speed = 0;
+    uint8_t accel = 255;
+    uint32_t pulses = 10 * FULL_ROT;
+
+    int16_t dir = 1;
+
     while (1)
     {
-        printf("loop\n");
+        if (speed >= 100 || speed <= -100)
+            dir = -dir;
+
+        speed = speed + 10 * dir;
+        ESP_LOGI(TAG, "speed: %d\n", speed);
+
+        emm42_servo_uart_move(emm42_conf, 1, speed, accel, pulses);
+        ESP_LOGI(TAG, "l_enc: %f\n", emm42_servo_uart_read_encoder(emm42_conf, 1));
+        ESP_LOGI(TAG, "g_enc: %f\n", emm42_servo_uart_read_motor_pos(emm42_conf, 1));
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+
+        mks_servo_uart_cr_set_pos(mks_conf, 2, speed, accel, pulses);
+        ESP_LOGI(TAG, "g_enc: %f\n", mks_servo_uart_read_encoder(mks_conf, 2));
+
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
